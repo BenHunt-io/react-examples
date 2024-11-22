51732127e7e427bd21df3075da1dd5df3108a466
ben <me@benhunt.io>
2024-11-22 14:54:29 -0600

    feat: add example where you can only call hooks from top level

--- src/pages/hooks/OnlyCallHooksFromTopLevel.tsx
+++ src/pages/hooks/OnlyCallHooksFromTopLevel.tsx
@@ -1,15 +1,36 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
 import Layout from '../../Layout';
+import { Button } from 'react-bootstrap';
 
 
-const IntroducingHooks = () => {
+const OnlyCallHooksFromTopLevel = () => {
+
+  const [count, setCount] = useState(0); // for triggering re-renders
+  const [firstName] = useState('Mary');
+
+  // Triggers an error on rerender, but only because react detects hooks are called in different order.
+  if(count === 0){
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(function useEffect1() {
+    console.log(`useEffect1 firstName: ${firstName}`);
+    });
+  }
+
+  const [lastName] = useState('Poppins');
+
+
+  useEffect(function updateTitle() {
+    console.log(`updateTitle firstName: ${firstName} lastName: ${lastName}`);
+  });
 
 
     return (
         <Layout>
-            Introducing Hooks
+            <Button onClick={() => setCount(count + 1)}>Re-render</Button>
         </Layout>
     )
 }
 
 
-export default IntroducingHooks
+export default OnlyCallHooksFromTopLevel
